c6ef927e9a416a7dc4b83a171cd379f23f2c4b19
Bo Herrmannsen <bo.herrmannsen@gmail.com>
2019-12-19 12:27:55 +0100

    Update wificheck.sh

--- wificheck.sh
+++ wificheck.sh
@@ -8,13 +8,16 @@ wlan='wlan0'
 pingip='8.8.8.8'
 ##################################################################
 
+echo -n "Current time : $(date) "
 echo "Performing Network check for $wlan"
 /bin/ping -c 1 -I $wlan $pingip > /dev/null 2> /dev/null
 if [ $? -ge 1 ] ; then
+    echo -n "Current time : $(date) "
     echo "Network connection down! Attempting reconnection."
     ip link set wlan0 down
     sleep 5
     ip link set wlan0 up
 else
+    echo -n "Current time : $(date) "
     echo "Network is Okay"
 fi
