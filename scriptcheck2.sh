19ac2b872b1720daecc7a75e2e5c279908b3262f
Bo Herrmannsen <bo.herrmannsen@gmail.com>
2019-12-19 14:37:50 +0100

    Update scriptcheck2.sh

--- scriptcheck2.sh
+++ scriptcheck2.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 if pgrep -f "python3 /home/pi/pool/poolweb.py" &>/dev/null; then
+    echo -n "Current time : $(date) "
     echo "Web interface running"
     exit
 else
+    echo -n "Current time : $(date) "
     echo "starting web interface"
     stdbuf -oL sudo python3 /home/pi/pool/poolweb.py >/dev/null  &
 fi
