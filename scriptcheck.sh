8e82726e1e91beb6274cd0be15da561569437644
Bo Herrmannsen <bo.herrmannsen@gmail.com>
2019-12-19 14:37:28 +0100

    Update scriptcheck.sh

--- scriptcheck.sh
+++ scriptcheck.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 if pgrep -f "python3 /home/pi/pool/poolmain.py" &>/dev/null; then
+    echo -n "Current time : $(date) "
     echo "Main script running"
     exit
 else
+    echo -n "Current time : $(date) "
     echo "starting main script"
     stdbuf -oL sudo python3 /home/pi/pool/poolmain.py >/dev/null  &
 fi
