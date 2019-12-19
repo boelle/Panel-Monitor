4593bb0b66f47e3be1cee7f0612a61768061fd6e
Bo Herrmannsen <bo.herrmannsen@gmail.com>
2019-12-19 13:51:01 +0100

    Update downsize_scriptcheck2log.sh

--- downsize_scriptcheck2log.sh
+++ downsize_scriptcheck2log.sh
@@ -5,8 +5,8 @@ wc -l scriptchek2log | read lcnt other
 if [ $lcnt -gt 2017 ] ; then
    ((start=$lcnt-2015))
    echo 'downsizing ...'
-   tail +$start /scriptchek2log > log.txtN
-   mv log.txtN /scriptchek2log
+   tail +$start scriptchek2log > log.txtN
+   mv log.txtN scriptchek2log
 fi
 
 exit 0
