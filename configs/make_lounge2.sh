b424eb870f1423f7294d7510908bfc4ed14f29fc
Bo Herrmannsen <bo.herrmannsen@gmail.com>
2025-06-05 10:29:34 +0200

    Update make_lounge2.sh

--- configs/make_lounge2.sh
+++ configs/make_lounge2.sh
@@ -1,6 +1,6 @@
-udo cp /home/pi/configs/lounge2/hostname/hosts /etc/
-sudo cp /home/pi/configs/lounge2/hostname/hostname /etc/
-sudo cp /home/pi/configs/lounge2/scriptconfig/config.py /home/pi/pool
-sudo cp /home/pi/configs/lounge2/scriptconfig/poolweb.py /home/pi/pool
-sudo cp /home/pi/configs/lounge2/scriptconfig/poolmain.py /home/pi/pool
-sudo cp -r /home/pi/configs/lounge2/tailscale/. /var/lib/tailscale/
+udo cp /home/pi/pool/configs/lounge2/hostname/hosts /etc/
+sudo cp /home/pi/pool/configs/lounge2/hostname/hostname /etc/
+sudo cp /home/pi/pool/configs/lounge2/scriptconfig/config.py /home/pi/pool
+sudo cp /home/pi/pool/configs/lounge2/scriptconfig/poolweb.py /home/pi/pool
+sudo cp /home/pi/pool/configs/lounge2/scriptconfig/poolmain.py /home/pi/pool
+sudo cp -r /home/pi/pool/configs/lounge2/tailscale/. /var/lib/tailscale/
