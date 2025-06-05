cd733254dd2757bf6770ebcac047b3c5c32adb06
Bo Herrmannsen <bo.herrmannsen@gmail.com>
2025-06-05 10:29:59 +0200

    Update make_shed1.sh

--- configs/make_shed1.sh
+++ configs/make_shed1.sh
@@ -1,6 +1,6 @@
-sudo cp /home/pi/configs/shed1/hostname/hosts /etc/
-sudo cp /home/pi/configs/shed1/hostname/hostname /etc/
-sudo cp /home/pi/configs/shed1/scriptconfig/config.py /home/pi/pool
-sudo cp /home/pi/configs/shed1/scriptconfig/poolweb.py /home/pi/pool
-sudo cp /home/pi/configs/shed1/scriptconfig/poolmain.py /home/pi/pool
-sudo cp -r /home/pi/configs/shed1/tailscale/. /var/lib/tailscale/
+sudo cp /home/pi/pool/configs/shed1/hostname/hosts /etc/
+sudo cp /home/pi/pool/configs/shed1/hostname/hostname /etc/
+sudo cp /home/pi/pool/configs/shed1/scriptconfig/config.py /home/pi/pool
+sudo cp /home/pi/pool/configs/shed1/scriptconfig/poolweb.py /home/pi/pool
+sudo cp /home/pi/pool/configs/shed1/scriptconfig/poolmain.py /home/pi/pool
+sudo cp -r /home/pi/pool/configs/shed1/tailscale/. /var/lib/tailscale/
