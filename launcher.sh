6252ff1357e554656d9e0c3ce0a362f159d41d04
Bo Herrmannsen <bo.herrmannsen@gmail.com>
2025-09-18 11:02:02 +0200

    Delete launcher.sh

--- launcher.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# launcher.sh
-# navigate to home directory, then to this directory, then execute python script
-activate() {
-. /home/pi/ha_mqtt_discoverable/bin/activate
-}
-activate
-python3 /home/pi/pool/poolmain.py &
