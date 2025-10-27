8ac5021635ada3492e0188da1dc98fa06551f750
Bo Herrmannsen <bo.herrmannsen@gmail.com>
2025-10-27 12:21:16 +0100

    Update make_lounge1.sh

--- configs/make_lounge1.sh
+++ configs/make_lounge1.sh
@@ -2,5 +2,4 @@ sudo cp /home/pi/pool/configs/lounge1/hostname/hosts /etc/
 sudo cp /home/pi/pool/configs/lounge1/hostname/hostname /etc/
 sudo cp /home/pi/pool/configs/lounge1/scriptconfig/config.py /home/pi/pool
 sudo cp /home/pi/pool/configs/lounge1/scriptconfig/poolweb.py /home/pi/pool
-sudo cp /home/pi/pool/configs/lounge1/scriptconfig/poolmain.py /home/pi/pool
 sudo cp -r /home/pi/pool/configs/lounge1/tailscale/. /var/lib/tailscale/
