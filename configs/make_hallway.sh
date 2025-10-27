99bc0edcf8d9c9947466dba1a1346c4e83d8ef7e
Bo Herrmannsen <bo.herrmannsen@gmail.com>
2025-10-27 12:21:00 +0100

    Update make_hallway.sh

--- configs/make_hallway.sh
+++ configs/make_hallway.sh
@@ -2,5 +2,4 @@ sudo cp /home/pi/pool/configs/hallway/hostname/hosts /etc/
 sudo cp /home/pi/pool/configs/hallway/hostname/hostname /etc/
 sudo cp /home/pi/pool/configs/hallway/scriptconfig/config.py /home/pi/pool
 sudo cp /home/pi/pool/configs/hallway/scriptconfig/poolweb.py /home/pi/pool
-sudo cp /home/pi/pool/configs/hallway/scriptconfig/poolmain.py /home/pi/pool
 sudo cp -r /home/pi/pool/configs/hallway/tailscale/. /var/lib/tailscale/
