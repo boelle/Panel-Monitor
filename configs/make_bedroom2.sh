48ab02acb6bfa0352d7edb2d380cd387279c4538
Bo Herrmannsen <bo.herrmannsen@gmail.com>
2025-10-27 12:20:41 +0100

    Update make_bedroom2.sh

--- configs/make_bedroom2.sh
+++ configs/make_bedroom2.sh
@@ -2,5 +2,4 @@ sudo cp /home/pi/pool/configs/bedroom2/hostname/hosts /etc/
 sudo cp /home/pi/pool/configs/bedroom2/hostname/hostname /etc/
 sudo cp /home/pi/pool/configs/bedroom2/scriptconfig/config.py /home/pi/pool
 sudo cp /home/pi/pool/configs/bedroom2/scriptconfig/poolweb.py /home/pi/pool
-sudo cp /home/pi/pool/configs/bedroom2/scriptconfig/poolmain.py /home/pi/pool
 sudo cp -r /home/pi/pool/configs/bedroom2/tailscale/. /var/lib/tailscale/
